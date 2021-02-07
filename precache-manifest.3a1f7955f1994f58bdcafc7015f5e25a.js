d2f40e6fd0fbc2e9928d391b11fbe5cfc6767f03
michaelxh <michaelxuhuang@gmail.com>
2021-02-06 20:45:48 -0500

    Updates

---
+++ precache-manifest.3a1f7955f1994f58bdcafc7015f5e25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8016b1dc83bc2d171d59395e5e6b532a",
+    "url": "/gloom-cards/index.html"
+  },
+  {
+    "revision": "bc69b37f5bf431294e2c",
+    "url": "/gloom-cards/static/css/main.6ca67d54.chunk.css"
+  },
+  {
+    "revision": "c83d96a818d342e02f73",
+    "url": "/gloom-cards/static/js/2.2bdcdeab.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/gloom-cards/static/js/2.2bdcdeab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc69b37f5bf431294e2c",
+    "url": "/gloom-cards/static/js/main.ddfb47f5.chunk.js"
+  },
+  {
+    "revision": "8d053f3fc167e7abeaca",
+    "url": "/gloom-cards/static/js/runtime-main.0828667e.js"
+  }
+]);
